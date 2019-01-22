3b92d6cdf6c6709b923cc742ff8f532fb710df39
Satanarayan Yadav <satyanarayan.y@gmail.com>
2019-01-22 13:41:02 +0530

    Initial Commit

---
+++ src/main/java/org/srysoft/mongodb/example/SpringMongodbApplication.java
@@ -0,0 +1,14 @@
+package org.srysoft.mongodb.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbApplication.class, args);
+	}
+
+}
+
