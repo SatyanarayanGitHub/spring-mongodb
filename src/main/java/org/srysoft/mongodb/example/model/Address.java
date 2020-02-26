336addc0d91b60e12422d8f3adb9ebcca2440c51
Satyanarayan <satyanarayan.y@gmail.com>
2020-02-26 20:02:53 +0530

    Model classes
    add lombok features

--- src/main/java/org/srysoft/mongodb/example/model/Address.java
+++ src/main/java/org/srysoft/mongodb/example/model/Address.java
@@ -1,22 +1,22 @@
 package org.srysoft.mongodb.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  * 
  * @author SATYA
  *
  */
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 	private String city;
 	private String country;
 
-	protected Address() {
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public String getCountry() {
-		return country;
-	}
 }
