84b27b3b333235e7a0b92642601f001c67c37651
Satyanarayan <satyanarayan.y@gmail.com>
2020-02-26 19:42:19 +0530

    Add Model/Documents

---
+++ src/main/java/org/srysoft/mongodb/example/model/Address.java
@@ -0,0 +1,22 @@
+package org.srysoft.mongodb.example.model;
+
+/**
+ * 
+ * @author SATYA
+ *
+ */
+public class Address {
+	private String city;
+	private String country;
+
+	protected Address() {
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+}
